8ce715e0bc3f7e59058b2f54ee7a0c85ff2e622b
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-05 18:14:16 +0530

    feat(ui): Implement Bot Chatbubble component
    * This component is used to display the bot response.
    * Display an avatar, name and the response.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import BotChatBubble from "@/components/shared/chatbubble/BotChatBubble";
+
 const Page = () => {
   return (
-    <div className="w-full p-36 m-10 rounded-[30px] bg-dark-500 text-light-800 flex flex-1">
-      <h1>Pal</h1>
+    <div className="w-full p-36 px-5 m-10 rounded-[30px] bg-dark-500 text-light-800 flex flex-1">
+      <BotChatBubble />
     </div>
   );
 };
