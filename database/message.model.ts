073841919fc13fc9db49ba84a4246561dcc383cd
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-07 22:28:18 +0530

    refactor: Update Message model schema
    * Prevents issues when references to yet-unestablished models exist.

--- database/message.model.ts
+++ database/message.model.ts
@@ -1,26 +1,29 @@
+// @ts-nocheck
+
 import { Schema, models, model, Document } from "mongoose";
 
 // Interface for the Message model
 export interface IMessage extends Document {
-  messageId: string;
   conversationId: string;
   sender: string;
-  type: string;
   content: string;
   timestamp: Date;
 }
 
 // Define the Message schema
 const MessageSchema = new Schema({
-  messageId: { type: String, required: true, unique: true }, // Ensure unique message IDs
-  conversationId: { type: String, required: true, ref: "Conversation" }, // Reference the Conversation model
+  conversationId: {
+    type: String,
+    required: true,
+  }, // Reference the Conversation model
   sender: { type: String, required: true },
-  type: { type: String, required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
 // Create the model
-const Message = models.Message || model("Message", MessageSchema);
+// @ts-ignore
+// const Message = models.Message || model("Message", MessageSchema);
+const Message = model<IMessage>("Message", MessageSchema);
 
 export default Message;
