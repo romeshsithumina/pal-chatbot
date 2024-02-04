6f7065bff93fb4b42092d0ad7da678da3d3b61a3
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-04 18:41:45 +0530

    feat(schema): Add Prisma schema
    * Added a new Prisma schema file defining models for User, Conversation, and Message for establishing relationships between them.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    mdxRs: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "http",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
