ed7c0cc161eb7d8cb2682f3a0c1d30a65f56ed0c
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-09 04:44:37 +0530

    fix(ui): resolve hydration error

---
+++ components/shared/ClientOnly.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+// Using to fix a error happens when the user interact with
+// the webpage while its refreshing
+import { useEffect, useState } from "react";
+import * as React from "react";
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
